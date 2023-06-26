34e7394e3d0d31b234f8addc3856c2ac32aff570
wbs <1134954328@qq.com>
2023-06-26 22:49:42 +0800

    实现 items 分页

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    items = Item.page(params[:page]).per(10)
+    render json: { resource: items, pager: {
+      page: params[:page],
+      per_page: 10,
+      count: Item.count,
+    } }
+  end
+
+  def create
+    item = Item.new amount: 1
+    if item.save
+      render json: { resource: item }
+    else
+      render json: { errors: item.errors }
+    end
+  end
+end
