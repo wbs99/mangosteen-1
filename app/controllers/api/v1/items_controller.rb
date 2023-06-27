90da9153cd3c0eb0d18fa033264abc8ec804866d
wbs <1134954328@qq.com>
2023-06-27 20:47:38 +0800

    添加 items controller 的测试

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.page(params[:page]).per(10)
-    render json: { resource: items, pager: {
+    items = Item.page(params[:page])
+    render json: { resources: items, pager: {
       page: params[:page],
       per_page: 10,
       count: Item.count,
@@ -9,7 +9,7 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new amount: 1
+    item = Item.new amount: params[:amount]
     if item.save
       render json: { resource: item }
     else
