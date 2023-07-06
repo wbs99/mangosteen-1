bbb56e540d216f18844eb79e6c03587fb62595b7
wbs <1134954328@qq.com>
2023-07-06 15:12:18 +0800

    给 item 添加 kind 字段

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe "Items", type: :request do
       get '/api/v1/items'
       expect(response).to have_http_status 401
     end
-
     it "分页" do
       user1 = User.create email: '1@qq.com'
       user2 = User.create email: '2@qq.com'
@@ -21,7 +20,6 @@ RSpec.describe "Items", type: :request do
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 10
-
       get '/api/v1/items?page=2', headers: user1.generate_auth_header
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
@@ -33,7 +31,8 @@ RSpec.describe "Items", type: :request do
       item2 = Item.create amount: 100, created_at: '2018-01-02', user_id: user1.id
       item3 = Item.create amount: 100, created_at: '2019-01-01', user_id: user1.id
 
-      get '/api/v1/items?created_after=2018-01-01&created_before=2018-01-03', headers: user1.generate_auth_header
+      get '/api/v1/items?created_after=2018-01-01&created_before=2018-01-03', 
+        headers: user1.generate_auth_header
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 2
@@ -44,7 +43,8 @@ RSpec.describe "Items", type: :request do
       user1 = User.create email: '1@qq.com'
       item1 = Item.create amount: 100, created_at: '2018-01-01', user_id: user1.id
 
-      get '/api/v1/items?created_after=2018-01-01&created_before=2018-01-02', headers: user1.generate_auth_header
+      get '/api/v1/items?created_after=2018-01-01&created_before=2018-01-02',
+        headers: user1.generate_auth_header
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
@@ -54,8 +54,8 @@ RSpec.describe "Items", type: :request do
       user1 = User.create email: '1@qq.com'
       item1 = Item.create amount: 100, created_at: '2018-01-01', user_id: user1.id
       item2 = Item.create amount: 100, created_at: '2017-01-01', user_id: user1.id
-
-      get '/api/v1/items?created_after=2018-01-01', headers: user1.generate_auth_header
+      get '/api/v1/items?created_after=2018-01-01', 
+        headers: user1.generate_auth_header
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
@@ -65,23 +65,30 @@ RSpec.describe "Items", type: :request do
       user1 = User.create email: '1@qq.com'
       item1 = Item.create amount: 100, created_at: '2018-01-01', user_id: user1.id
       item2 = Item.create amount: 100, created_at: '2019-01-01', user_id: user1.id
-      
-      get '/api/v1/items?created_before=2018-01-02', headers: user1.generate_auth_header
+
+      get '/api/v1/items?created_before=2018-01-02', 
+        headers: user1.generate_auth_header
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
       expect(json['resources'][0]['id']).to eq item1.id
     end
   end
-  describe "create" do
-    xit "can create an item" do 
+  describe "创建账目" do
+    it '未登录创建' do 
+      post '/api/v1/items', params: { amount: 100 }
+      expect(response).to have_http_status 401
+    end
+    it "登录后创建" do 
+      user = User.create email: '1@qq.com'
       expect {
-        post '/api/v1/items', params: {amount: 99}
+        post '/api/v1/items', params: {amount: 99}, headers: user.generate_auth_header
       }.to change {Item.count}.by 1
       expect(response).to have_http_status 200
       json = JSON.parse response.body
       expect(json['resource']['id']).to be_an(Numeric)
       expect(json['resource']['amount']).to eq 99
+      expect(json['resource']['user_id']).to eq user.id
     end
   end
 end
