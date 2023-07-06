6d866bd67df9c530dff668b7d48f2f15521aa739
wbs <1134954328@qq.com>
2023-07-06 23:18:36 +0800

    优化部署到云服务器和宿主机的部署脚本

--- bin/pack_for_remote.sh
+++ bin/pack_for_remote.sh
@@ -2,15 +2,16 @@
 user=mangosteen
 ip=121.5.55.95
 
-time=$(date +'%Y%m%d-%H%M%S')            # 获取当前时间
+time=$(date +'%Y%m%d-%H%M%S')
 cache_dir=tmp/deploy_cache
-dist=$cache_dir/mangosteen-$time.tar.gz  # 获取压缩包文件名
-current_dir=$(dirname $0)                # 获取当前目录，在这就是 bin 目录
-deploy_dir=/home/$user/deploys/$time     # 远程部署目录
+dist=$cache_dir/mangosteen-$time.tar.gz
+current_dir=$(dirname $0)
+deploy_dir=/home/$user/deploys/$time
 gemfile=$current_dir/../Gemfile
 gemfile_lock=$current_dir/../Gemfile.lock
 vendor_dir=$current_dir/../vendor
 vendor_1=rspec_api_documentation
+api_dir=$current_dir/../doc/api
 
 function title {
   echo 
@@ -28,10 +29,8 @@ title "打包本地依赖 ${vendor_1}"
 bundle cache --quiet
 tar -cz -f "$vendor_dir/cache.tar.gz" -C ./vendor cache
 tar -cz -f "$vendor_dir/$vendor_1.tar.gz" -C ./vendor $vendor_1
-
 title '创建远程目录'
 ssh $user@$ip "mkdir -p $deploy_dir/vendor"
-
 title '上传源代码和依赖'
 scp $dist $user@$ip:$deploy_dir/
 yes | rm $dist
@@ -41,15 +40,13 @@ scp -r $vendor_dir/cache.tar.gz $user@$ip:$deploy_dir/vendor/
 yes | rm $vendor_dir/cache.tar.gz
 scp -r $vendor_dir/$vendor_1.tar.gz $user@$ip:$deploy_dir/vendor/
 yes | rm $vendor_dir/$vendor_1.tar.gz
-
 title '上传 Dockerfile'
 scp $current_dir/../config/host.Dockerfile $user@$ip:$deploy_dir/Dockerfile
-
 title '上传 setup 脚本'
 scp $current_dir/setup_remote.sh $user@$ip:$deploy_dir/
-
+title '上传 API 文档'
+scp -r $api_dir $user@$ip:$deploy_dir/
 title '上传版本号'
 ssh $user@$ip "echo $time > $deploy_dir/version"
-
 title '执行远程脚本'
 ssh $user@$ip "export version=$time; /bin/bash $deploy_dir/setup_remote.sh"
