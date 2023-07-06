01611e8855648abdc1ff2543901c3a59d3b77076
wbs <1134954328@qq.com>
2023-07-06 22:58:52 +0800

    修复远程部署脚本

--- bin/pack_for_remote.sh
+++ bin/pack_for_remote.sh
@@ -9,7 +9,8 @@ current_dir=$(dirname $0)                # 获取当前目录，在这就是 bin
 deploy_dir=/home/$user/deploys/$time     # 远程部署目录
 gemfile=$current_dir/../Gemfile
 gemfile_lock=$current_dir/../Gemfile.lock
-vendor_cache_dir=$current_dir/../vendor/cache
+vendor_dir=$current_dir/../vendor
+vendor_1=rspec_api_documentation
 
 function title {
   echo 
@@ -20,23 +21,35 @@ function title {
 }
 
 
-title '打包源代码为压缩文件'
-mkdir $cache_dir
-bundle cache
-tar --exclude="tmp/cache/*" --exclude="tmp/deploy_cache/*" -czv -f $dist *
+mkdir -p $cache_dir
+title '打包源代码'
+tar --exclude="tmp/cache/*" --exclude="tmp/deploy_cache/*" --exclude="vendor/*" -cz -f $dist *
+title "打包本地依赖 ${vendor_1}"
+bundle cache --quiet
+tar -cz -f "$vendor_dir/cache.tar.gz" -C ./vendor cache
+tar -cz -f "$vendor_dir/$vendor_1.tar.gz" -C ./vendor $vendor_1
+
 title '创建远程目录'
 ssh $user@$ip "mkdir -p $deploy_dir/vendor"
-title '上传压缩文件'
+
+title '上传源代码和依赖'
 scp $dist $user@$ip:$deploy_dir/
 yes | rm $dist
 scp $gemfile $user@$ip:$deploy_dir/
 scp $gemfile_lock $user@$ip:$deploy_dir/
-scp -r $vendor_cache_dir $user@$ip:$deploy_dir/vendor/
+scp -r $vendor_dir/cache.tar.gz $user@$ip:$deploy_dir/vendor/
+yes | rm $vendor_dir/cache.tar.gz
+scp -r $vendor_dir/$vendor_1.tar.gz $user@$ip:$deploy_dir/vendor/
+yes | rm $vendor_dir/$vendor_1.tar.gz
+
 title '上传 Dockerfile'
 scp $current_dir/../config/host.Dockerfile $user@$ip:$deploy_dir/Dockerfile
+
 title '上传 setup 脚本'
 scp $current_dir/setup_remote.sh $user@$ip:$deploy_dir/
+
 title '上传版本号'
 ssh $user@$ip "echo $time > $deploy_dir/version"
+
 title '执行远程脚本'
 ssh $user@$ip "export version=$time; /bin/bash $deploy_dir/setup_remote.sh"
