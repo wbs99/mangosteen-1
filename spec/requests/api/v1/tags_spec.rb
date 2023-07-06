efc72b70a10eba3e33f2b9aa90a9704e0663e2ee
wbs <1134954328@qq.com>
2023-07-06 10:17:39 +0800

    完成 post tags api

--- spec/requests/api/v1/tags_spec.rb
+++ spec/requests/api/v1/tags_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Api::V1::Tags", type: :request do
       get '/api/v1/tags'
       expect(response).to have_http_status(401)
     end
-    
+
     it "登录后获取标签" do
       user = User.create email: '1@qq.com'
       another_user = User.create email: '2@qq.com'
@@ -22,6 +22,35 @@ RSpec.describe "Api::V1::Tags", type: :request do
       expect(response).to have_http_status(200)
       json = JSON.parse response.body
       expect(json['resources'].size).to eq 1
+    end
+  end
+
+  describe '创建标签' do
+    it '未登录创建标签' do
+      post '/api/v1/tags', params: {name: 'x', sign: 'x'}
+      expect(response).to have_http_status(401)
+    end
+    it '登录后创建标签' do
+      user = User.create email: '1@qq.com'
+      post '/api/v1/tags', params: {name: 'name', sign: 'sign'}, headers: user.generate_auth_header
+      expect(response).to have_http_status(200)
+      json = JSON.parse response.body
+      expect(json['resource']['name']).to eq 'name'
+      expect(json['resource']['sign']).to eq 'sign'
+    end
+    it '登录后创建标签失败，因为没填 name' do
+      user = User.create email: '1@qq.com'
+      post '/api/v1/tags', params: {sign: 'sign'}, headers: user.generate_auth_header
+      expect(response).to have_http_status(422)
+      json = JSON.parse response.body
+      expect(json['errors']['name'][0]).to eq "can't be blank"
+    end
+    it '登录后创建标签失败，因为没填 sign' do
+      user = User.create email: '1@qq.com'
+      post '/api/v1/tags', params: {name: 'name'}, headers: user.generate_auth_header
+      expect(response).to have_http_status(422)
+      json = JSON.parse response.body
+      expect(json['errors']['sign'][0]).to eq "can't be blank"
     end
   end
 end
