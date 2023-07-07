dd297f546ba59e6872c9268f95f81f929b43446f
wbs <1134954328@qq.com>
2023-07-07 11:09:31 +0800

    更新邮件发送代码和配置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,10 +56,22 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "mangosteen_1_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.qq.com",
+    port: 587,
+    domain: "smtp.qq.com",
+    user_name: "1134954328@qq.com",
+    password: Rails.application.credentials.email_password,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    open_timeout: 10,
+    read_timeout: 10,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
