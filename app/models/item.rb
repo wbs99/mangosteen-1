4a16cab673e23f0ef4df1d2860b247804e7b6e66
wbs <1134954328@qq.com>
2023-07-11 10:44:49 +0800

    将 happen_at 重命名为 happened_at，并兼容 happen_at 的输出

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,30 +1,31 @@
 class Item < ApplicationRecord
   paginates_per 25
-  enum kind: {expenses: 1, income: 2 }
-  validates :amount, numericality: { other_than: 0 }
+  default_scope { where(deleted_at: nil).order(happen_at: :desc) }
+  enum kind: { expenses: 1, income: 2 }
   validates :amount, presence: true
+  validates :amount, numericality: { other_than: 0 }
   validates :kind, presence: true
-  validates :tag_ids, presence: true
   validates :happen_at, presence: true
+  validates :tag_ids, presence: true
 
   belongs_to :user
-  
-  validate :check_tags_id_belong_to_user
 
-  def check_tags_id_belong_to_user
+  validate :check_tag_ids_belong_to_user
+
+  alias_attribute :happen_at, :happened_at
+
+  def check_tag_ids_belong_to_user
     all_tag_ids = Tag.where(user_id: self.user_id).map(&:id)
     if self.tag_ids & all_tag_ids != self.tag_ids
-      self.errors.add :tag_ids, '不属于当前用户'
+      self.errors.add :tag_ids, "不属于当前用户"
     end
   end
 
-  # 根据 id 查找 tag 表中对应的 tag
   def tags
     Tag.where(id: tag_ids)
   end
 
-  # 不显示删除的 item
-  def self.default_scope
-    where(deleted_at: nil)
+  def serializable_hash(options = {})
+    super(options.merge(methods: [:happen_at], include: [:tags]))
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_021905) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_024144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_021905) do
     t.integer "amount"
     t.text "notes"
     t.bigint "tag_ids", array: true
-    t.datetime "happen_at"
+    t.datetime "happened_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "kind", null: false
