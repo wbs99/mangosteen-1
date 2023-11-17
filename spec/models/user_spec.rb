6aa07295560f72c66779739f35632e3ecfff70b2
wbs <1134954328@qq.com>
2023-11-17 16:28:24 +0800

    remove spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  it '有 email' do
-    user = User.new email:'1134954328@qq.com'
-    expect(user.email).to eq '1134954328@qq.com'
-  end
-end
