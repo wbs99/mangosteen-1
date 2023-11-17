feaf009faadcc97da1b1b27a44a82aeec0a140d4
wbs <1134954328@qq.com>
2023-11-17 16:27:40 +0800

    validation_code model format

--- app/models/validation_code.rb
+++ app/models/validation_code.rb
@@ -11,6 +11,7 @@ class ValidationCode < ApplicationRecord
   def generate_code
     self.code = SecureRandom.random_number.to_s[2..7]
   end
+  
   def send_email
     UserMailer.welcome_email(self.email)&.deliver
   end
