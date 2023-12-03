187e755c4532fe2eba0174bc217f8ec3eb36c9d1
wbs <1134954328@qq.com>
2023-12-03 16:35:33 +0800

    tag model 添加注释

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,11 @@
 class Tag < ApplicationRecord  
   paginates_per 25
-  default_scope { where(deleted_at: nil).order(created_at: :desc) }
   enum kind: {expenses: 1, income: 2 }
   validates :name, presence: true,length: { maximum: 4 }
   validates :sign, presence: true
   validates :kind, presence: true
   belongs_to :user
+
+  # 查询 tag 时，会默认加上 deleted_at 为空，并且按照 created_at 降序
+  default_scope { where(deleted_at: nil).order(created_at: :desc) }
 end
